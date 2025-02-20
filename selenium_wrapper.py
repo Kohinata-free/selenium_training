5140eb311b14b7076b2e11d9501d7f099e00cf86
Kohinata-free <kohhy777@gmail.com>
2025-02-20 10:56:44 +0900

    不要なインポートを削除

--- selenium_wrapper.py
+++ selenium_wrapper.py
@@ -2,7 +2,7 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-import keyboard
+# import keyboard
 
 class SeleniumTypingPage:
     def OpenTypingPage():
